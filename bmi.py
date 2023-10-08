6e1e5fbf8a5441d573ce48c415e6b75c2d3abded
Daniel Beránek <daniel.beranek96@gmail.com>
2023-10-08 22:27:51 +0200

    multiline string +fstring

--- bmi.py
+++ bmi.py
@@ -1,32 +1,18 @@
 #bmi kalkulacka
-vaha = float(input("zadej svoji hmotnost(v kg)"))
+vaha = int(input("zadej svoji hmotnost(v kg)"))
 vyska = float(input("zadej svoji vysku (v m)"))
 vyska_na_druhou = (vyska * vyska) 
-bmi = vaha / vyska_na_druhou               #pokud chceme BMI s desetinným místem, napíšeme float místo int
+bmi = float(vaha / vyska_na_druhou)
 
 if bmi < 18.5:
-    print("Vase BMI je ",bmi,"\n"
-    "Mate podvahu, zacnete vice jist\n"
-    "Spocitejte si doporuceny denni kaluricky prijem\n"
-    "Jezte pravidelne a alespon 5x denne\n"
-    "Neopominejte dostatecny a kvalitni spanek")
+    print(bmi) 
+    print("Mate podvahu, zacnete vice jist")
 elif bmi <= 24.9:
-    print("Vase BMI je ",bmi,"\n"
-    "Vase vaha je v norme\n"
-    "Dodrzujte pravidelny pohyb, stravovaci navyky\n"
-    "Nezapominejte na zdravy spankovy rezim")
+    print(bmi)
+    print("Vase vaha je v norme")
 elif bmi <= 29.9:
-    print("Vase BMI je ",bmi,"\n"
-    "Mate nadvahu, zacnete se vice hybat\n"
-    "Omezte vysokokaloricka jidla\n"
-    "Vyhybejte se piti slazenych napoju\n"
-    "Nastavte si zdravy spankovy rezim")
+    print(bmi)
+    print("Mate nadvahu, zacnete se vice hybat")
 elif bmi >= 30:
-    print("Vase BMI je ",bmi,"\n"
-    "Vyhledejte sveho obvodniho lekare\n"
-    "Vynechte vysokokaloricka jidla a slazene napoje\n"
-    "Zaradte pohyb do vaseho denniho rezimu\n"
-    "Pravidelne spete alespon 8 hodin\n"
-    "Nepijte alkohol a nekurte")
-# f string - dohledat
-# více řádkový string
+    print(bmi)
+    print("Vyhledejte sveho obvodniho lekare")
