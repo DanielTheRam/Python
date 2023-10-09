1a929ab4a2eaa24d77dda629006c7a68f8d22aed
Daniel Beránek <daniel.beranek96@gmail.com>
2023-10-09 18:39:11 +0200

    fstring + multiline string

--- bmi.py
+++ bmi.py
@@ -1,18 +1,34 @@
 #bmi kalkulacka
-vaha = int(input("zadej svoji hmotnost(v kg)"))
+vaha = float(input("zadej svoji hmotnost(v kg)"))
 vyska = float(input("zadej svoji vysku (v m)"))
 vyska_na_druhou = (vyska * vyska) 
-bmi = float(vaha / vyska_na_druhou)
-
+bmi = vaha / vyska_na_druhou               #pokud chceme BMI s desetinným místem, napíšeme float místo int
+hlaska_bmi = f"Vase bmi je {bmi}"
+podvaha =   """Mate podvahu, zacnete vice jist
+               Spocitejte si doporuceny denni kaluricky prijem
+               Jezte pravidelne a alespon 5x denne
+               Neopominejte dostatecny a kvalitni spanek"""
+norma =     """Vase vaha je v norme
+               Dodrzujte pravidelny pohyb, stravovaci navyky
+               Nezapominejte na zdravy spankovy rezim"""
+nadvaha =   """Mate nadvahu, zacnete se vice hybat
+               Omezte vysokokaloricka jidla
+               Vyhybejte se piti slazenych napoju
+               Nastavte si zdravy spankovy rezim"""
+obezita =   """Vyhledejte sveho obvodniho lekare
+               Vynechte vysokokaloricka jidla a slazene napoje
+               Zaradte pohyb do vaseho denniho rezimu
+               Pravidelne spete alespon 8 hodin
+               Nepijte alkohol a nekurte"""
 if bmi < 18.5:
-    print(bmi) 
-    print("Mate podvahu, zacnete vice jist")
+    print(hlaska_bmi, podvaha)
+
 elif bmi <= 24.9:
-    print(bmi)
-    print("Vase vaha je v norme")
+    print(hlaska_bmi, norma)
 elif bmi <= 29.9:
-    print(bmi)
-    print("Mate nadvahu, zacnete se vice hybat")
+    print(hlaska_bmi, nadvaha)
 elif bmi >= 30:
-    print(bmi)
-    print("Vyhledejte sveho obvodniho lekare")
+    print(hlaska_bmi, obezita)
+    
+# f string - dohledat
+# více řádkový string
--- kamennuzkypapir.py
+++ kamennuzkypapir.py
@@ -1,8 +1,7 @@
-# KAMEN NUZKY PAPIR
-moznosti = ("kamen", "nuzky", "papir")
+import random                           #toto a 5. radek jsem jediny googlil, konvence se píší na první řádek
 
+moznosti = ("kamen", "nuzky", "papir")
 hrac = input("napiste 'kamen', 'nuzky', nebo 'papir': ")
-import random                           #toto a 6. radek jsem jediny googlil
 pocitac = random.choice(moznosti)
 print(pocitac)
 
@@ -10,19 +9,15 @@ if hrac == "kamen" and pocitac == "nuzky":
     print("vyhrals")
 elif hrac == "kamen" and pocitac == "papir":
     print("prohrals")
-elif hrac == "kamen" and pocitac == "kamen":
-    print("remiza")
 elif hrac == "nuzky" and pocitac == "papir":
-    print("vyhra")
+    print("vyhrals")
 elif hrac == "nuzky" and pocitac == "kamen":
     print("prohrals")
-elif hrac == "nuzky" and pocitac == "nuzky":
-    print("remiza")
 elif hrac == "papir" and pocitac == "kamen":
     print("vyhrals")
 elif hrac == "papir" and pocitac == "nuzky":
     print("prohrals")
-elif hrac == "papor" and pocitac == "papir":
-    print("remiza")
+elif hrac == pocitac:
+    print("remiza - oba jste zvolili stejnou moznost")
 else:
     print("spatny vyraz")
