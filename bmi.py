67f6616b756caa411e9533444a3051e77f193c69
Daniel Beránek <daniel.beranek96@gmail.com>
2023-10-13 17:19:56 +0200

    odebrani komentáře

--- bmi.py
+++ bmi.py
@@ -3,7 +3,7 @@ vaha = float(input("zadej svoji hmotnost(v kg)"))
 vyska = float(input("zadej svoji vysku (v m)"))
 vyska_na_druhou = (vyska * vyska) 
 bmi = vaha / vyska_na_druhou               #pokud chceme BMI s desetinným místem, napíšeme float místo int
-hlaska_bmi = f"Vase bmi je {bmi}"
+hlaska_bmi = f"Vase bmi je {bmi}"           # jen jednu proměnnou
 podvaha =   """Mate podvahu, zacnete vice jist
                Spocitejte si doporuceny denni kaluricky prijem
                Jezte pravidelne a alespon 5x denne
@@ -14,7 +14,7 @@ norma =     """Vase vaha je v norme
 nadvaha =   """Mate nadvahu, zacnete se vice hybat
                Omezte vysokokaloricka jidla
                Vyhybejte se piti slazenych napoju
-               Nastavte si zdravy spankovy rezim"""                 #vložit hlaska_bmi a kategorii pod jeden f string
+               Nastavte si zdravy spankovy rezim"""                 
 obezita =   """Vyhledejte sveho obvodniho lekare
                Vynechte vysokokaloricka jidla a slazene napoje
                Zaradte pohyb do vaseho denniho rezimu
@@ -30,5 +30,3 @@ elif bmi <= 29.9:
 elif bmi >= 30:
     print(hlaska_bmi, obezita)
     
-# f string - dohledat
-# více řádkový string
