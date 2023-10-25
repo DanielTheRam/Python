4d3b09b022e4c8706a786cddbd57cb4259acac11
DanielTheRam <daniel.beranek96@gmail.com>
2023-10-25 20:06:12 +0200

    zjednoduseni kodu

--- ctvrtahodina.py/knihovna.py
+++ ctvrtahodina.py/knihovna.py
@@ -1,79 +1,35 @@
+def get_volne_knizky(borrowed_books):
+    available_books = []
+    for book in borrowed_books:
+        if borrowed_books[book] == "":
+            available_books.append(book)
+    return available_books
+
+borrowed_books = {"Veronika decides to die": "Karel Novák", "Alchemist": "", "Animal farm": "Zuzana Malátová", "1982": "Marie Sýkorová", "R.U.R.": "Adam Dítě"}
 name = input("Napište svoje jméno ")
-borrowed_books = {"Veronika decides to die": "Karel Novák", "Alchemist": "Jiří Svoboda", "Animal farm": "Zuzana Malátová", "1982": "Marie Sýkorová", "R.U.R.": "Adam Dítě"}
-available_books = {"Lord of the Rings": 1, "Hobbit": 3, "Angels and Demons": 2, "Little Prince": 6}
-users_books = {"Dekameron"}
 while True:
-    if name == "admin":
-        action = input("""Vyberte číslo operace, kterou chcete provést:
-                       1 - Vaše půjčené knihy
-                       2 - Zobrazit seznam dostupných knih
-                       3 - Vratit knihu
-                       4 - Pujcit si knihu
-                       5 - Zobrazit seznam všech pujcenych knih """)
-        if action =="1":
-            if not users_books:
-                print("Nemáte žádnou půjčenou knihu")
-            else:
-                print(f"Vaše knihy jsou: {users_books}")
-        elif action == "2":
-            print(f"Seznam dostupných knih/počet: {available_books} ")
-        elif action == "3":
-                if not users_books:
-                      print("Nemáte půjčenou žádnou knihu")
-                else: 
-                    rbook = input(f"Jakou knihu chcete vrátit?{users_books} ")
-                    if rbook in users_books:
-                        rbook = users_books.pop()
-                        available_books[rbook] =+ 1
-                        print(f"Vaše půjčené knihy jsou: {users_books}")
-                    elif not rbook in users_books:
-                        print("Takovou knihu půjčenou nemáte")
-        elif action == "4":
-                gbook = input(f"Jakou knihu si chcete půjčit? {available_books} ")
-                if gbook in available_books:
-                      users_books.add(gbook)
-                      del available_books[gbook]
-                      borrowed_books[gbook] = name
-                      print(f"Vaše půjčené knihy jsou: {users_books}")
-                else:
-                      print("Tato kniha není k dispozici")
-        elif action =="5":
-                print(f"Seznam půjčených knih: {borrowed_books}")
-        else: 
-                print("Špatně zadané číslo operace")
-    else:
-        action = input("""Napište číslo operace, kterou chcete provést:
-                        1 - Vaše půjčené knihy
-                        2 - Zobrazit seznam dostupných knih 
-                        3 - Vratit knihu 
-                        4 - Pujcit si knihu """)
-        if action =="1":
-            if not users_books:
-                print("Nemáte žádnou půjčenou knihu")
-            else:
-                print(f"Vaše knihy jsou: {users_books}")
-        elif action == "2":
-                print(f"Seznam dostupných knih/počet: {available_books} ")
-        elif action == "3":
-                if not users_books:
-                      print("Nemáte půjčenou žádnou knihu")
-                else: 
-                    rbook = input(f"Jakou knihu chcete vrátit?{users_books} ")
-                    if rbook in users_books:
-                        rbook = users_books.pop()
-                        available_books[rbook] =+ 1
-                        print(f"Vaše půjčené knihy jsou: {users_books}")
-                    elif not rbook in users_books:
-                        print("Takovou knihu půjčenou nemáte")
-        elif action == "4":
-                gbook = input(f"Jakou knihu si chcete půjčit? {available_books} ")
-                if gbook in available_books:
-                      users_books.add(gbook)
-                      available_books[gbook] -= 1
-                      print(f"Vaše půjčené knihy jsou: {users_books}")
-                else:
-                      print("Tato kniha není k dispozici")
+    volne_knizky = get_volne_knizky(borrowed_books)
+    action = input("""Vyberte číslo operace, kterou chcete provést:
+                    1 - Zobrazit seznam knih
+                    2 - Vratit knihu
+                    3 - Pujcit si knihu""")
+    if action =="1":
+        if name == "admin":
+                print(f"Seznam knih: {borrowed_books.keys()}")
         else:
-            print("Špatně zadaný výraz")
-# pokud si uživatel půjčí knihu, kterou poté vrátí, není odebrán ze seznamu půjčených knih
-# kdy používám del/pop/remote
+            print(f"Seznam knih: {get_volne_knizky(borrowed_books)}")
+    elif action == "2":
+        kniha = input("Jakou knihu chcete vrátit? ")
+        if kniha in borrowed_books:
+             borrowed_books[kniha] = ""
+        else:
+            print("Kniha je asi jiné knihovny lol.")
+    elif action == "3":
+        gbook = input(f"jakou knihu si chcete půjčit? {get_volne_knizky(borrowed_books)}")
+        if gbook in borrowed_books:
+             borrowed_books[gbook] = name
+        else: 
+             print("Kniha není dostupná")    
+    else: 
+        print("špatně zadané číslo operace")
+print(get_volne_knizky())
