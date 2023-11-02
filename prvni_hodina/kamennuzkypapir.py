df9b7a3978296d60c36a5a2701e2d3a19eed7e1c
DanielTheRam <daniel.beranek96@gmail.com>
2023-11-02 20:05:11 +0100

    odebrání duplikace kódu

--- prvni_hodina/kamennuzkypapir.py
+++ prvni_hodina/kamennuzkypapir.py
@@ -1,50 +1,24 @@
-# KAMEN NUZKY PAPIR
+import random                                              
 moznosti = ("kamen", "nuzky", "papir")
-
-hrac = input("napiste 'kamen', 'nuzky', nebo 'papir': ")
-import random                           #toto a 6. radek jsem jediny googlil
-pocitac = random.choice(moznosti)
-print(pocitac)
-
-if hrac == "kamen" and pocitac == "nuzky":
-    print("vyhrals")
-elif hrac == "kamen" and pocitac == "papir":
-    print("prohrals")
-elif hrac == "kamen" and pocitac == "kamen":
-    print("remiza")
-elif hrac == "nuzky" and pocitac == "papir":
-    print("vyhra")
-elif hrac == "nuzky" and pocitac == "kamen":
-    print("prohrals")
-elif hrac == "nuzky" and pocitac == "nuzky":
-    print("remiza")
-elif hrac == "papir" and pocitac == "kamen":
-    print("vyhrals")
-elif hrac == "papir" and pocitac == "nuzky":
-    print("prohrals")
-elif hrac == "papor" and pocitac == "papir":
-    print("remiza")
-else:
-    print("spatny vyraz")
-from random import choice                    
-moznosti = ("kamen", "nuzky", "papir")
-hrac = input("napiste 'kamen', 'nuzky', nebo 'papir': ")
-pocitac = choice(moznosti)
-print(pocitac)
-if hrac == "kamen" and pocitac == "nuzky":
-    print("vyhrals")
-elif hrac == "kamen" and pocitac == "papir":
-    print("prohrals")
-elif hrac == "nuzky" and pocitac == "papir":
-    print("vyhrals")
-elif hrac == "nuzky" and pocitac == "kamen":
-    print("prohrals")
-elif hrac == "papir" and pocitac == "kamen":
-    print("vyhrals")
-elif hrac == "papir" and pocitac == "nuzky":
-    print("prohrals")
-elif hrac == pocitac:
-    print("remiza - oba jste zvolili stejnou moznost")
-else:
-    print("spatny vyraz")
+while True:
+    hrac = input("napiste 'kamen', 'nuzky', nebo 'papir': ")
+    from random import choice 
+    pocitac = choice(moznosti)
+    print(pocitac)
+    if hrac == "kamen" and pocitac == "nuzky":
+        print("vyhrals")
+    elif hrac == "kamen" and pocitac == "papir":
+        print("prohrals")
+    elif hrac == "nuzky" and pocitac == "papir":
+        print("vyhrals")
+    elif hrac == "nuzky" and pocitac == "kamen":
+        print("prohrals")
+    elif hrac == "papir" and pocitac == "kamen":
+        print("vyhrals")
+    elif hrac == "papir" and pocitac == "nuzky":
+        print("prohrals")
+    elif hrac == pocitac:
+        print("remiza - oba jste zvolili stejnou moznost")
+    else:
+        print("spatny vyraz")
 
