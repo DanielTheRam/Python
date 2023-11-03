71dc4e19b8cead58cd0c993d73dce33f8b87eb51
DanielTheRam <daniel.beranek96@gmail.com>
2023-11-03 20:50:03 +0100

    přejmenování složek

--- druha_hodina/indexovani.py
+++
@@ -1,8 +0,0 @@
-veta = "mame doma kocku, je chlupata a mam ji rad"
-print(veta[0])
-print(veta[12])
-print(veta[0:3]) #0, 1, 2 a třetí se nepočítá 
-print(veta[:3])
-print(veta[3:])
-print(veta[0:8:2]) #každé druhé
-print(veta[0:0:-1]) #vypíše pozpátku
--- treti_hodina/nasobic.py
+++
@@ -1,3 +0,0 @@
-from math import pi
-uzivatel = int(input("Zadejte jakékoliv číslo "))
-print(pi * uzivatel) 
