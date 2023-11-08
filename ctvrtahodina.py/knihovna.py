4c5f8909a98c710f1d337bd3cd70ca77ec2e378a
DanielTheRam <daniel.beranek96@gmail.com>
2023-11-08 16:43:46 +0100

    uprava hesel + prestupne roky

--- ctvrtahodina.py/knihovna.py
+++ ctvrtahodina.py/knihovna.py
@@ -1,35 +1,37 @@
-def get_volne_knizky(borrowed_books):
-    available_books = []
-    for book in borrowed_books:
-        if borrowed_books[book] == "":
-            available_books.append(book)
-    return available_books
-
-borrowed_books = {"Veronika decides to die": "Karel Novák", "Alchemist": "", "Animal farm": "Zuzana Malátová", "1982": "Marie Sýkorová", "R.U.R.": "Adam Dítě"}
+all_books = {"Veronika decides to die": "Karel Novák", 
+                  "Alchemist": "", 
+                  "Animal farm": "Zuzana Malátová",
+                  "1982": "Marie Sýkorová", 
+                  "R.U.R.": "Adam Dítě",
+                  "Clown": "",
+                  "Miracle": "",
+                  "My fair lady": ""}
 name = input("Napište svoje jméno ")
+tveknihy = ""
+dostupne = {kniha: jmeno for kniha, jmeno in all_books.items() if jmeno == ""}
 while True:
-    volne_knizky = get_volne_knizky(borrowed_books)
     action = input("""Vyberte číslo operace, kterou chcete provést:
                     1 - Zobrazit seznam knih
                     2 - Vratit knihu
                     3 - Pujcit si knihu""")
     if action =="1":
         if name == "admin":
-                print(f"Seznam knih: {borrowed_books.keys()}")
+                print(f"Seznam knih: {all_books}")
         else:
-            print(f"Seznam knih: {get_volne_knizky(borrowed_books)}")
+            print(f'Seznam knih: {dostupne}')
     elif action == "2":
         kniha = input("Jakou knihu chcete vrátit? ")
-        if kniha in borrowed_books:
-             borrowed_books[kniha] = ""
+        if kniha in all_books:
+             all_books[kniha] = ""
         else:
             print("Kniha je asi jiné knihovny lol.")
     elif action == "3":
-        gbook = input(f"jakou knihu si chcete půjčit? {get_volne_knizky(borrowed_books)}")
-        if gbook in borrowed_books:
-             borrowed_books[gbook] = name
+        gbook = input(f"jakou knihu si chcete půjčit? ")
+        if gbook in all_books:
+             all_books[gbook] = name
+             tveknihy += gbook
+             print(f"Vaše knihy jsou: {tveknihy}")
         else: 
              print("Kniha není dostupná")    
     else: 
         print("špatně zadané číslo operace")
-print(get_volne_knizky())
--- generator_hesel.py
+++ generator_hesel.py
@@ -5,21 +5,30 @@ while True:
                         2. Střední
                         3. Silné
                         4. Neprůstřelné """)
-    slabe = ("abcdefghijklmnoprqstuvwxyz")
-    stredni = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    slabe = ("abcdefghijklmnoprqstuvwxyz")  #
+    stredni = ("ABCDEFGHIJKLMNOPQRSTUVWXYZ")#
     silne = ("0123456789")
-    heslo = ""
-    neprustrelne = (",.-!?:_/*-+") 
+    neprustrelne = (",.-!?:_/*-+")
+    heslo = "" 
     if obtiznost == "1":
-            for _ in range(7):
-                slabeheslo = random.choice(slabe)
-                heslo += slabeheslo
-            print(f"Vaše heslo je: {heslo}")
-    if obtiznost == "2":
-            for _ in range(3):
-                 slabeheslo = random.choice(slabe)
-                 heslo += slabeheslo
-            for _ in range(3):
-                  stredniheslo = random.choice(stredni)
-                  heslo += stredniheslo
-            print(f"Vaše heslo je{heslo}")
+        for _ in range(7):
+            slabeheslo = random.choice(slabe)
+            heslo += slabeheslo
+        print(f"Vaše heslo je: {heslo}")
+    elif obtiznost == "2":
+        znaky = list(slabe + stredni)
+        random.shuffle(znaky)
+        heslo = "".join(random.choice(znaky) for _ in range(7))
+        print(f"Vaše heslo je: {heslo}")
+    elif obtiznost == "3":
+        znaky = list(slabe + stredni + silne)
+        random.shuffle(znaky)
+        heslo = "".join(random.choice(znaky) for _ in range(10))
+        print(f"Vaše heslo je: {heslo}")
+    elif obtiznost == "4":
+        znaky = list(slabe + stredni + silne + neprustrelne)
+        random.shuffle(znaky)
+        heslo = "".join(random.choice(znaky) for _ in range(12))
+        print(f"Vaše heslo je: {heslo}")
+    else:
+          ("Zadali jste špatné číslo obtížnosti hesla")
